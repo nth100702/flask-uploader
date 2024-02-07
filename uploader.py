f79b88b4c66cb325082254d47a62f58bbae87ca6
IT/LuanNVM <luannvm@gemadept.corp>
2024-02-07 13:42:33 +0700

    implemented feat: files could be uploaded to local server

--- uploader.py
+++ uploader.py
@@ -10,12 +10,15 @@ from cryptography.fernet import Fernet
 from cryptography.hazmat.primitives.asymmetric import rsa
 import logging
 from datetime import datetime
+from flask import redirect, request, session
+import asyncio
 
 # Filepaths
 # os.path.join("uploads", "photos")
 # os.path.join("uploads", "video")
 # resolve path to thianhdep_submissions.csv in ./data/thianhdep_submissions.csv
 data_filepath = os.path.join("data", "thianhdep_submissions.csv")
+media_filepath = os.path.join("uploads")
 # print(data_filepath)
 
 
@@ -39,13 +42,17 @@ class UploadForm(FlaskForm):
     photos = MultipleFileField(
         "Photos",
         validators=[
-            FileAllowed(["jpg", "png", "zip"]),
-            FileMaxSizeMB(1),
-            # FileRequired(),
+            FileAllowed(["jpg", "jpeg", "png", "gif"]),
+            FileMaxSizeMB(50),
+            FileRequired(),
         ],
     )  # FileMaxSizeMB(5), FileRequired()
     video = FileField(
-        "Video", validators=[FileAllowed(["mp4"]), FileMaxSizeMB(500)]
+        "Video",
+        validators=[
+            FileAllowed(["mp4", "mov", "avi", "wmv", "flv", "mkv"]),
+            FileMaxSizeMB(500),
+        ],
     )  # FileMaxSizeMB(500)
     submit = SubmitField(label="Submit")
 
@@ -68,40 +75,88 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = "your_secret_key"
 app.config["UPLOADED_FILES_DEST"] = "./uploads"
 
+# handle media file uploads
+async def handle_media_files(employee_id, full_name, photos, video):
+    # generate timestamp: `YYMMDDHHmmss`
+    timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+    # generate directory name: `uploads/employee_id_timestamp`
+    dirname = f"{employee_id}_{full_name}"
+    # create directory if not exists
+    media_submission = os.path.join(media_filepath, dirname)
+    os.makedirs(media_submission, exist_ok=True)
+    # handle photos, write to media_submission
+    if photos is not None:
+        await asyncio.gather(
+            *(save_file(photo, media_submission, timestamp) for photo in photos)
+        )
+    # handle video
+    if video is not None:
+        # save video to media_submission with this format: `{timestamp}_{filename}`
+        await save_file(video, media_submission, timestamp)
+
+
+async def save_file(file, media_submission, timestamp):
+    if file is not None and file.filename != "":
+        filename = f"{timestamp}_{secure_filename(file.filename)}"
+        file.save(os.path.join(media_submission, filename))
+
+
 # Error handling
 # display error message to user as an html element
 """
-for each submit, check if the employee_id exists in the csv file
-if exists, check if photos and video are the same as the previous ones
-if not, append the new photos and video to the existing record
-if same, do nothing
-if not, create a new record
+for each submit, create a new record in the csv file
+    only latest records are valid
 """
 
 
 # Check form_data input
-def submit_handler(form_data):
+async def submit_handler(form_data: dict):
     employee_id = form_data["employee_id"]
     full_name = form_data["full_name"]
     department = form_data["department"]
     company = form_data["company"]
-    # photos = form_data['photos']
-    # video = form_data['video']
+    photos = form_data["photos"]
+    video = form_data["video"]
     # generate timestamp for each submission
     created = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    print("photos", photos)
 
     with open(data_filepath, "a", newline="") as file:
         writer = csv.writer(file)
         # write new record (row) to csv file
         writer.writerow([employee_id, full_name, department, company, created])
 
+    # handle media files
+    await handle_media_files(employee_id, full_name, photos, video)
+
+
+"""Notes:
+Current behavior: When the user uploads a file different from the allowed file types, pressing Submit will do nothing
+    Expected behavior: An error message should be displayed to the user
+"""
+
 
 @app.route("/", methods=["GET", "POST"])
-def upload():
+def upload_file():
     form = UploadForm()
-    if form.validate_on_submit():
-        submit_handler(form.data)
-    return render_template("upload.html", form=form)
+    notif = session.pop("notif", None)
+    # Since / route is both GET and POST, we need to handle each case separately
+    if request.method == "POST":
+        if form.validate_on_submit():
+            # print('data_filepath', data_filepath)
+            # print('form.data', form.data)
+            asyncio.run(submit_handler(form.data))
+            return redirect("/success")  # Redirect to success.html
+        # error handling
+        else:
+            session["notif"] = form.errors
+    return render_template("upload.html", form=form, notif=notif)
+
+
+# /success route
+@app.route("/success")
+def success():
+    return render_template("success.html")
 
 
 if __name__ == "__main__":
