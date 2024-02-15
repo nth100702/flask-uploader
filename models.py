aff496c52df0afccf09214e80c643499cf5c346f
IT/LuanNVM <luannvm@gemadept.corp>
2024-02-15 14:38:01 +0700

    set up sqlite3 with sqlalchemy sdk

--- models.py
+++ models.py
@@ -1,5 +1,3 @@
-# Define flask-alchemy models: ChunkedFile
-
 from app import db
 from datetime import datetime
 
@@ -13,17 +11,32 @@ class ChunkedFile(db.Model):
     filename = db.Column(db.String(100), nullable=False)
     filepath = db.Column(db.String(100), nullable=False)
     date_uploaded = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-
-class FormData(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    ma_nhan_vien = db.Column(db.String(100), nullable=False)
-    ho_ten = db.Column(db.String(100), nullable=False)
-    ma_don_vi = db.Column(db.String(100), nullable=False)
-
-# specify model relationships
-# each entry has many chunked files
-# each chunked file belongs to one entry
-
-
+    submit_record_id = db.Column(db.Integer, db.ForeignKey('submit_record.id'), nullable=False)
+    # specify relationship to SubmitRecord
+    submit_record = db.relationship('SubmitRecord', backref='chunked_files')
     def __repr__(self):
-        return f"ChunkedFile('{self.filename}', '{self.filepath}', '{self.date_uploaded}', '{self.ma_nhan_vien}', '{self.ho_ten}', '{self.ma_don_vi}', '{self.ten_tac_pham}')"
+        return f"ChunkedFile('{self.filename}', '{self.date_uploaded}')"
+
+class SubmitRecord(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    submitter_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    date_submitted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    # specify relationship to User
+    submitter = db.relationship('User', backref='submitter')
+    # specify relationship to ChunkedFile
+    chunked_files = db.relationship('ChunkedFile', backref='submit_record')
+    status = db.Column(db.String(100), nullable=False)
+    def __repr__(self):
+        return f"SubmitRecord('{self.date_submitted}', '{self.status}')"
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    employee_id = db.Column(db.String(100), nullable=False)
+    full_name = db.Column(db.String(100), nullable=False)
+    division = db.Column(db.String(100), nullable=False)
+    submit_records = db.relationship('SubmitRecord', backref='user')
+    date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    # specify relationship to SubmitRecord
+    submit_records = db.relationship('SubmitRecord', backref='user')
+    def __repr__(self):
+        return f"User('{self.employee_id}', '{self.full_name}', '{self.division}')"
